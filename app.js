4acc1151839370f818aca170118923933f010062
ibadurrehman718 <ibadurrehman718@gmail.com>
2023-08-23 04:10:50 +0500

    Arrays Added

--- app.js
+++ app.js
@@ -198,20 +198,17 @@
 //   }
 // }
 
-
 // Question = 1 New
 
 // Create an HTML file with a <p> element that has the id "myParagraph".
 // Using document.getElementById, select the paragraph element.
 // Change the text content of the paragraph to "Hello, DOM!"
 
-
 // Question = 2 New
 
 // Create an HTML file with a <div> element that has the id "myDiv".
 // Using document.getElementById, select the div element.
 // Change the background color of the div to "blue" when a button with the id "changeColorButton" is clicked.
-
 
 // // Question = 2 New
 
@@ -240,13 +237,49 @@
 //   }
 // }
 
-
 function Show() {
-  const Para = document.getElementById("Div")
+  const Para = document.getElementById("Div");
   if (Para.style.display === "none") {
-    Para.style.display = "block"
+    Para.style.display = "block";
   } else {
-    Para.style.display = "none"
+    Para.style.display = "none";
   }
-
 }
+
+// ! Arrays
+
+//! 1- Create an initial array called studentNames with the following student names in this order: "Alice", "Bob", "Charlie", "David", "Eve".
+
+//? 2- Insert the name "Frank" after "Charlie" in the studentNames array.
+
+//? 3- Remove "David" from the studentNames array.
+
+//? 4- Add "Grace" and "Helen" to the end of the studentNames array.
+
+//? 5- Replace "Bob" with "John" in the studentNames array.
+
+//? 6- Display the final studentNames array to the console.
+
+// * Answer:1/2
+
+// const Names = ["Alice", "Bob", "Charlie", "David", "Eve"];
+// Names.splice(3, 0, "Frank");
+// console.log(Names);
+
+// * Answer:3
+
+// const Names = ["Alice", "Bob", "Charlie", "Frank", "David", "Eve"];
+// Names.splice(4, 1);
+// console.log(Names);
+
+// * Answer:4
+
+// const Names = ["Alice", "Bob", "Charlie", "Frank", "David", "Eve"];
+// Names.push("Grace", "Helen");
+// console.log(Names);
+
+// * Answer:5
+
+// const Names = ["Alice", "Bob", "Charlie", "Frank", "David", "Eve"];
+// Names.splice(1, 1, "John");
+// console.log(Names);
